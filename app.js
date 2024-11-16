986e1ee37a49d12d973ca9ee26dcb1c8294c7af5
subrat singh <subratprivate777@gmail.com>
2024-11-16 22:33:02 +0530

    home page done

---
+++ app.js
@@ -0,0 +1,52 @@
+document.getElementById("card1").addEventListener("mouseenter", () => {
+    document.getElementById("card_hover1").style.top="30%";
+
+})
+document.getElementById("card1").addEventListener("mouseleave", () => {
+    document.getElementById("card_hover1").style.top="75%";
+})
+
+
+document.getElementById("card2").addEventListener("mouseenter", () => {
+    document.getElementById("card_hover2").style.top="30%";
+
+})
+document.getElementById("card2").addEventListener("mouseleave", () => {
+    document.getElementById("card_hover2").style.top="75%";
+})
+
+
+document.getElementById("card3").addEventListener("mouseenter", () => {
+    document.getElementById("card_hover3").style.top="30%";
+
+})
+document.getElementById("card3").addEventListener("mouseleave", () => {
+    document.getElementById("card_hover3").style.top="75%";
+})
+
+
+document.getElementById("card4").addEventListener("mouseenter", () => {
+    document.getElementById("card_hover4").style.top="30%";
+
+})
+document.getElementById("card4").addEventListener("mouseleave", () => {
+    document.getElementById("card_hover4").style.top="75%";
+})
+
+
+document.getElementById("card5").addEventListener("mouseenter", () => {
+    document.getElementById("card_hover5").style.top="30%";
+
+})
+document.getElementById("card5").addEventListener("mouseleave", () => {
+    document.getElementById("card_hover5").style.top="75%";
+})
+
+
+document.getElementById("card6").addEventListener("mouseenter", () => {
+    document.getElementById("card_hover6").style.top="30%";
+
+})
+document.getElementById("card6").addEventListener("mouseleave", () => {
+    document.getElementById("card_hover6").style.top="75%";
+})
